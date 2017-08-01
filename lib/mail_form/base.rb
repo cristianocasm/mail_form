a89e47dfe5ca0c649a03a4bf15664538723102d1
Cristiano Alencar <cristiano.souza.mendonca@gmail.com>
2017-08-01 06:29:02 -0300

    Adding dirty methods

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,7 +1,16 @@
 module MailForm
   class Base
+    include ActiveModel::AttributeMethods  # 1) attribute methods behavior
+    attribute_method_prefix "clear_"       # 2) "clear_" is attribute prefix
+
     def self.attributes *names
       attr_accessor *names
+      # 3) ask to define the prefix methods for the given attribute names
+      define_attribute_methods names
+    end
+
+    def clear_attribute(attribute)
+      send("#{attribute}=", nil)
     end
   end
 end
